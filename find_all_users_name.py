839d424148a426dadb907e51711d9bd2d7ca0b62
Bekzod1999 <developmentfirm1999@gmail.com>
2022-10-01 10:17:39 +0500

    userName

--- find_all_users_name.py
+++ find_all_users_name.py
@@ -1,30 +1,3 @@
-# from read_data import read_data
-
-# def find_all_users_name(data: dict)->list:
-#     """
-#     This function will find all the users in the json file and return the list of users name.
- 
-#     Parameters:
-#         data (dict): Dictionary containing the data of the json file.
-#     Returns:
-#         list: List containing all the users name.
-#     """
-#     users = []
-#     gather = data['messages']
-#     for i in range(len(gather)):
-#         gatherTwo = gather[i].items()
-#         for k, v in gatherTwo:
-#             if k == 'actor_id' or k == 'from_id':
-#                 users.append(v)
-#     separateUsers = set(users)
-#     return list(separateUsers), len(list(separateUsers))
-
-# data1 = read_data('data/result.json')
-# print(find_all_users_name(data1))
-
-
-
-
 from read_data import read_data
 
 def find_all_users_name(data: dict)->list:
@@ -41,7 +14,7 @@ def find_all_users_name(data: dict)->list:
     for i in range(len(gather)):
         gatherTwo = gather[i].items()
         for k, v in gatherTwo:
-            if k == 'actor_id' or k == 'from_id':
+            if k == 'actor' or k == 'from':
                 users.append(v)
 
     # I took all user name
@@ -52,4 +25,4 @@ def find_all_users_name(data: dict)->list:
     return usersTwo
 
 data1 = read_data('data/result.json')
-print(find_all_users_name(data1))
+print(find_all_users_name(data1)) 
